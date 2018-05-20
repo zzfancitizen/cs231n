ac06b67d3bae291f44ea9a2da6337a5c92b6468e
zzfancitizen <zzfancitizen@gmail.com>
2018-05-21 00:17:27 +0800

    update KNN

--- Assignment 1/KNN/cifair_10_pickle.py
+++ Assignment 1/KNN/cifair_10_pickle.py
@@ -24,7 +24,7 @@ def get_train(path):
         i += 1
 
     X_tra = np.array(X).reshape(50000, 3, 32, 32).transpose(0, 2, 3, 1)
-    Y_tra = np.array(Y).reshape(50000, 1).transpose(0, 1)
+    Y_tra = np.array(Y).reshape(50000, )
 
     return X_tra, Y_tra
 
@@ -37,29 +37,6 @@ def get_test(path):
     X_tst_temp = np.array(X_tst_temp)
     Y_tst_temp = np.array(Y_tst_temp)
     X_tst = X_tst_temp.reshape(10000, 3, 32, 32).transpose(0, 2, 3, 1)
-    Y_tst = Y_tst_temp.reshape(10000, 1).transpose(0, 1)
+    Y_tst = Y_tst_temp.reshape(10000, )
 
     return X_tst, Y_tst
-
-# if __name__ == '__main__':
-#
-#     Xtr, Ytr = get_train(PATH)
-#     Xte, Yte = get_test(PATH)
-#
-#     Xtr_rows = Xtr.reshape(Xtr.shape[0], 32 * 32 * 3)
-#     Xte_rows = Xte.reshape(Xte.shape[0], 32 * 32 * 3)
-#
-#     Xval_rows = Xtr_rows[:1000, :]
-#     Yval = Ytr[:1000]
-#     Xtr_rows = Xtr_rows[1000:, :]
-#     Ytr = Ytr[1000:]
-#
-#     nn = KNearestNeighbor()
-#     nn.train(Xtr_rows, Ytr)
-#
-#     validation_accuracies = []
-#
-#     for k in [1, 3, 5, 10, 20, 50, 100]:
-#         Yval_predict = nn.predict(Xval_rows, k)
-#         print('k: %i accuracy: %f' % (k, np.mean(Yval_predict == Yval)))
-#         validation_accuracies.append((k, np.mean(Yval_predict == Yval)))
