ec20f0b150cba513e8a320fe45e7ab5301380608
zzfancitizen <zzfancitizen@gmail.com>
2018-05-21 08:44:14 +0800

    delete TODO

--- assignment1/KNN/k_nearest_neighbor.py
+++ assignment1/KNN/k_nearest_neighbor.py
@@ -118,28 +118,8 @@ class KNearestNeighbor(object):
         num_test = dists.shape[0]
         y_pred = np.zeros(num_test)
         for i in range(num_test):
-            # A list of length k storing the labels of the k nearest neighbors to
-            # the ith test point.
             closest_y = []
-            #########################################################################
-            # TODO:                                                                 #
-            # Use the distance matrix to find the k nearest neighbors of the ith    #
-            # testing point, and use self.y_train to find the labels of these       #
-            # neighbors. Store these labels in closest_y.                           #
-            # Hint: Look up the function numpy.argsort.                             #
-            #########################################################################
             y_indices = np.argsort(dists[i, :], axis=0)
             closest_y = self.y_train[y_indices[:k]]
-            #########################################################################
-            # TODO:                                                                 #
-            # Now that you have found the labels of the k nearest neighbors, you    #
-            # need to find the most common label in the list closest_y of labels.   #
-            # Store this label in y_pred[i]. Break ties by choosing the smaller     #
-            # label.                                                                #
-            #########################################################################
             y_pred[i] = np.argmax(np.bincount(closest_y))
-            #########################################################################
-            #                           END OF YOUR CODE                            #
-            #########################################################################
-
         return y_pred
